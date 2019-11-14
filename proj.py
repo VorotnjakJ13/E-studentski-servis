36feaa428441a40adb3bc1d9e6c67f57d3bf7642
VorotnjakJ13 <julija.vorotnjak@outlook.com>
2019-11-14 12:14:26 +0100

    popravljen projekt

--- proj.py
+++ proj.py
@@ -1,40 +1,41 @@
-# coding: 'utf8'
+# -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import requests
+import requests 
+import re
 import csv
-# import re
+import pandas
+
 ############################################################
 url = 'https://www.studentski-servis.com/index.php?t=prostaDela&hp=true&page=1&perPage=3016&sort=1&workType=1&keyword='
-url_data = requests.get(url).content
-soup = BeautifulSoup(url_data,'html.parser', from_encoding='utf-8')
+       
+url_data=requests.get(url).content
+
+soup = BeautifulSoup(url_data,'html.parser')
+
 #############################################################
+## SEZNAMI:
 jobs = soup.findAll('div',{'class':'jobItem'})
 ime = soup.findAll('span', {'class':'title'})
 placa = soup.findAll('span',{'class':'postavka'})
 lokacija = soup.findAll('span',{'class':'lokacija'})
 opis = soup.findAll('div',{'class':'jobContent col'})
+opiss= soup.findAll('p')
+sifre=[]
+for i in range(len(jobs)):
+    sifra = jobs[i].li.parent.li.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling
+    sifre.append(sifra.text)
 
-dataset = [(a.text,b.text,c.text,d.text) for a,b,c,d in zip( ime,placa,lokacija,opis)]
+dataset= [(s,i.text,p.text,l.text,op.text) for s,i,p,l,op in zip( sifre,ime,placa,lokacija,opis)]
 
 
-with open('estudent.csv','w') as csvfile: 
+
+with open('estudent.csv','w+',encoding='utf-8') as csvfile: 
     writer = csv.writer(csvfile)
-    for touple in dataset:
-        for niz in touple:
-            
-            niz.replace('\u20ac', 'EUR')
-            niz.replace('\u010d','č' )
-            niz.replace('\u010c','Č')
-            niz.replace('\u0160','š')
-            niz.replace('\u0161','Š')
-            niz.replace('\u017d','Ž')
-            niz.replace('\u017d','ž')
-            niz.encode('utf-8')
-           
-            writer.writerow(touple).encode('utf-8',errors='strict')
-        
-
-
-
+   
+    for tuple in dataset:
+        if tuple[0] is csvfile.read():
+            None
+        else:
+            writer.writerow(tuple)
 
